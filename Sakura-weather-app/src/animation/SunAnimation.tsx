1d6169e1dc3a6ba5a078e05e98eacda5e181bfb4
Ilya Smakovskyi <iluhasmakovskyi@gmail.com>
2026-04-13 22:05:29 +0200

    add/sun-animation/by/tsparticles/but not working///MUST-To-Fix

--- Sakura-weather-app/src/animation/SunAnimation.tsx
+++ Sakura-weather-app/src/animation/SunAnimation.tsx
@@ -1,3 +1,51 @@
-export default function SunAnimation() {
-    return <div>Sun</div>;
+import Particles from "@tsparticles/react";
+// import { loadFull } from "tsparticles";
+// import { useCallback } from "react";
+// import type { Engine } from "tsparticles-engine";
+import { useMemo } from "react";
+import type { ISourceOptions } from "tsparticles-engine";
+
+export function MySunAnimation({ type }: { type: string }) {
+
+    // const particlesInit = useCallback(async (engine: Engine) => {
+    //     await loadFull(engine);
+    // }, []);
+
+    const getOptions = (type: string): ISourceOptions => {
+        if (type === "rain") {
+            return {
+                particles: {
+                    number: {
+                        value: 100
+                    },
+                    move: {
+                        enable: true,
+                        speed: 10,
+                        direction: "bottom"
+                    },
+                    shape: {
+                        type: "image",
+                        options: {
+                            image: {
+                                src: "https://particles.js.org/images/flower.svg",
+                                width: 32,
+                                height: 32
+                            }
+                        }
+                    }
+                }
+            };
+        }
+
+        return {
+            particles: {
+                number: {value: 0}
+            }
+        };
+    };
+
+
+    const options = useMemo(() => getOptions(type), [type]);
+
+    return <Particles options={options} />;
 }
